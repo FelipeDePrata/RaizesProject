a82f8ad8baacecf12ba49e35e27a94b80d5a554c
Felipe José <felipe.jose.rj@gmail.com>
2026-06-08 16:35:50 -0300

    criacao enum canalPedido

---
+++ src/Domain/entities/CanalPedido.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class CanalPedido(Enum):
+    APP = 1
+    TOTEM = 2
+    BALCAO = 3
+    PICKUP = 4
+    WEB = 5
--- src/Infrastructure/database/models/pedido_model.py
+++ src/Infrastructure/database/models/pedido_model.py
@@ -2,20 +2,12 @@ from sqlalchemy import Column, String, Integer, ForeignKey, Boolean, DateTime, F
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.sql import func
 from enum import Enum
+from Domain.entities import CanalPedido
 
 Base = declarative_base()
-
-class CanalPedido(Enum):
-    APP = 1
-    TOTEM = 2
-    BALCAO = 3
-    PICKUP = 4
-    WEB = 5
-
-class Pedido(Base):
+class PedidoModel(Base):
     __tablename__ = "pedidos" 
     id = Column("id",Integer, primary_key=True, autoincrement=True, unique=True)
-    data_hora = Column("data_hora", DateTime(timezone=True), server_default=func.now())
     status = Column("status", Boolean)
     valor_total = Column("valor_total", Float)
     canal_pedido = Column("canalPedido", SQLEnum(CanalPedido))
